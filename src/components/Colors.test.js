4e75c7ea0d3be56710ddd5aa8082cc4db73aecd4
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-15 11:55:57 -0700

    colors test passing

--- src/components/Colors.test.js
+++ src/components/Colors.test.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Colors from './Colors';
 
+describe('colors component', () => {
+  it('renders the colors', () => {
+    const colors = [{ name: 'red', rgb: { red: 255, green: 0, blue: 0 } }, { name: 'green', rgb: { red: 0, green: 255, blue: 0 } }, { name: 'blue', rgb: { red: 0, green: 0, blue: 255 } }];
+    const wrapper = shallow(<Colors colors={colors} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
