5d49c515518a498131147ea647a47a140295595d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-16 10:42:33 -0700

    app test passing but not working on localhost

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import Photo from './Photo';
 
 function Photos({ photos }) {
-  const photoList = photos.map(({ photo }) => (
-    <li key={photo}>
-      <Photo photo={photo} />
-    </li>
-  ));
+  const photoList = photos.map(photo  => {
+    return (
+      <li key={photo}>
+        <Photo photo={photo} />
+      </li>
+    );
+  });
 
   return (
     <ul>
