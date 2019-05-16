71ca42afac2c4541a5063655e5ad1908cec1fa1a
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-15 17:52:31 -0700

    added console logs onclick

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <>
       <Sidebar>
-        <nav>Child1</nav>
-        <nav>Child2</nav>
-        <nav>Child3</nav>
+        <nav onClick={() => console.log('clicked')}>Child1</nav>
+        <nav onClick={() => console.log('child2 clicked')}>Child2</nav>
+        <nav onClick={() => console.log('child3 clicked')}>Child3</nav>
       </Sidebar>
     </>
   );
