5b18bc9012ae14d10b3fc29554f0f8e64668738d
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-16 10:45:30 -0700

    photo test written and passing

--- src/components/Photo.test.js
+++ src/components/Photo.test.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+import Photo from './Photo';
+import images from '../assets/images.jpeg';
+
+describe('photo component', () => {
+  it('renders the photo', () => {
+    const wrapper = shallow(<Photo photo={images} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
