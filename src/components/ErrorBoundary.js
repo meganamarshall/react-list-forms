bf96f596603ad6cec6dd5e3ecdd72ec923513534
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-05-16 22:22:50 -0700

    errorBoundary component

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ErrorBoundary extends Component {
+  static propTypes = {
+    children: PropTypes.node
+  }
+
+  state = {
+    error: false
+  }
+
+  static getDerivedStateFromError() {
+    return { error: true };
+  }
+
+  componentDidCatch(error) {
+    console.log(error);
+  }
+
+  render() {
+    if(this.state.error) {
+      return <h3>Error</h3>;
+    }
+    return this.props.children;
+  }
+}
+
